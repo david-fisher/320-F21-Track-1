59d9db766f2f5522861b39ed14809a457e934021
Mehul1107 <43730457+Mehul1107@users.noreply.github.com>
2021-10-20 00:03:48 -0400

    Update Score.java

--- Objects/Score.java
+++ Objects/Score.java
@@ -17,4 +17,5 @@ public class Score {
         //The score will be updated based on the rules that will be provided by Team 4
         int new_score = 1;//Calculate based on rules and update
         return new_score;
+    }
 }
