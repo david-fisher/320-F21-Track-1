c35fab48589ceb511298a4a7a0112344f3ed9939
Mehul1107 <43730457+Mehul1107@users.noreply.github.com>
2021-10-20 00:03:26 -0400

    Update Score.java

--- Objects/Score.java
+++ Objects/Score.java
@@ -1,5 +1,20 @@
 package Objects;
 
 public class Score {
+    final int ID; int cur_score;
     
+    public Player(int id){
+        this.ID = id;
+        this.cur_score = 0;
+    }
+    
+    
+    public int get_score(){
+        return this.cur_score:
+    }
+    
+    public void update(ArrayList<Rule> rules){
+        //The score will be updated based on the rules that will be provided by Team 4
+        int new_score = 1;//Calculate based on rules and update
+        return new_score;
 }
