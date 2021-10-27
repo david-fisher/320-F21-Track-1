0164852b7ab0e09de09f7097b685efde00b9814f
Spencer <89930870+Sad-0w@users.noreply.github.com>
2021-10-27 12:49:00 -0400

    Adding file structure changes

---
+++ src/RngUI/RNG.java
@@ -0,0 +1,28 @@
+-import java.io.*;
+import java.util.Arrays;
+
+class RNG {
+    String type;
+    int quantity, min, max;
+    Token deck;
+
+    GameToken() {
+        this.type = null;
+        this.name = "undefined";
+        this.quantity = 0;
+        this.value = 0;
+        this.movement = null;
+        this.visuals = null;
+        this.cards = new Token[0];
+    }
+
+    GameToken(String newType, String newName, int newQuantity, int newValue, Pair<String, int>[] newMovement, Object newVisuals) {
+        this.type = newType;
+        this.name = newName;
+        this.quantity = newQuantity;
+        this.movement = newMovement;
+        this.visuals = newVisuals;
+        this.cards = new Token[0];
+    }
+
+}
