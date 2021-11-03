4ecf0bb54a2b03a4c02dd24f7c93e7a986724377
sasana-uwu <sasalabrina@gmail.com>
2021-11-03 13:09:26 -0400

    Update Rule.java

--- Objects/Rule.java
+++ Objects/Rule.java
@@ -1,8 +1,59 @@
+import java.util.*;
 
 public class Rule {
     final String ID;
+    final String rule; int score; String type; RNG rng;
 
-    public Rule(String id){
-        this.ID = id;
+    public Rule(){
+        this.ID = UUID.randomUUID().toString();
+        this.score = 0;
+        this.type = "";
+        this.rule = "";
+        this.rng = new RNG();
     }
+
+    public Rule(rule){
+        this.ID = UUID.randomUUID().toString();
+        this.score = 0;
+        this.type = "";
+        this.rule = rule;
+        this.rng = new RNG();
+    }
+
+    public Rule(rule, score){
+        this.ID = UUID.randomUUID().toString();
+        this.score = score;
+        this.type = "";
+        this.rule = rule;
+        this.rng = new RNG();
+    }
+
+    public Rule(rule, score, type){
+        this.ID = UUID.randomUUID().toString();
+        this.score = score;
+        this.type = type;
+        this.rule = rule;
+        this.rng = new RNG();
+    }
+
+    public Rule(rule, score, type, rng){
+        this.ID = UUID.randomUUID().toString();
+        this.score = score;
+        this.type = type;
+        this.rule = rule;
+        this.rng = rng;
+    }
+
+    public Rule(id, rule, score, type, rng){
+        this.ID = id;
+        this.score = score;
+        this.type = type;
+        this.rule = rule;
+        this.rng = rng;
+    }
+
+    public String get_id() { return this.id; }
+
+    public RNG get_rng() { return this.rng; }
+
 }
