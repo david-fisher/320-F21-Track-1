ba49a9c362224678d495b5161a96707947783307
Vinisha Dhuleshia <vinishadhuleshia@1x-nat-vl930-172-30-81-248.wireless.umass.edu>
2021-11-10 14:13:29 -0500

    added controller class for rule editor

---
+++ src/RuleEditorUI/Controller.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
