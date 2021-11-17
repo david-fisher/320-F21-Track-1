0669b6e4821949b9d0d5063780c367ffc1346570
Haijie Qin <jackqin2000@gmail.com>
2021-11-17 18:29:02 -0500

    Fix intellij running issue. Fix ModeMenuController.java package issue

--- MainMenuSceneBuilder/src/ModeMenuController.java
+++ MainMenuSceneBuilder/src/ModeMenuController.java
@@ -1,7 +1,14 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ModeMenuController {
     @FXML
