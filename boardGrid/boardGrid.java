4ad7930bee000a4749426e9d45a61f1942ae2e0d
Haijie Qin <jackqin2000@gmail.com>
2021-11-17 21:01:10 -0500

    Implement Marius

--- boardGrid/boardGrid.java
+++ boardGrid/boardGrid.java
@@ -48,7 +48,7 @@ public class boardGrid {
                 }
 
                 // if it's an empty stack pane, then create some default features
-                currentCell.loadCellImage("images/fish.jpeg");
+                currentCell.loadCellImage(i % 2 == 0? "images/fish.jpeg": "images/marius.jpeg");
                 currentCell.setIndex(i + j * width + 1);
                 currentRow.add(currentCell);
             }
