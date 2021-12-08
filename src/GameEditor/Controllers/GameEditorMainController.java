886be53e0f85fecf4e2fafa9b5ec8480628156b2
Nate Grebelsky <nate.greb@gmail.com>
2021-12-08 13:07:29 -0500

    refactoring unnecessary code and exit to main menu function

--- src/GameEditor/Controllers/GameEditorMainController.java
+++ src/GameEditor/Controllers/GameEditorMainController.java
@@ -1,139 +1,24 @@
 package GameEditor.Controllers;
 
-import Objects.JSONConverter;
-import Objects.Token;
+import Objects.*;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import mainMenu.Main;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.*;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 
 public class GameEditorMainController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private ComboBox<String> dropdown0, dropdown1, dropdown2, dropdown3, dropdown4;
-    @FXML
-    private Button transition;
-    @FXML
-    private Button addTransition;
-    @FXML
-    private HBox modifyTransitionButtons;
-    @FXML
-    private Button changeTransition;
-    @FXML
-    private Button deleteTransition;
-    
-    private String[] transitions = {"\u2192", "\u2190", "\u2194"};
-    private int currentTransition = 0;
-    
-    @FXML
-    private ComboBox<String>[] dropdowns = new ComboBox[5];
-    private int numVisible = 1;
-    private int[] visible = { 1, 0,0,0,0};
-    private String[] deleteButtons = { "d0", "d1", "d2", "d3", "d4" };
-    private double orgSceneX, orgSceneY;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    void initializeTurnRule() {
-        dropdowns[0] = dropdown0;
-        dropdowns[1] = dropdown1;
-        dropdowns[2] = dropdown2;
-        dropdowns[3] = dropdown3;
-        dropdowns[4] = dropdown4;
-        dropdowns[1].getParent().setManaged(false);;
-        dropdowns[2].getParent().setManaged(false);;
-        dropdowns[3].getParent().setManaged(false);;
-        dropdowns[4].getParent().setManaged(false);;
-    }
-
-    @FXML
-    void addDropDown(MouseEvent event) {
-        if (numVisible < 5) {
-            int hidden;
-            for (int i = 0; i < visible.length; i++) {
-                if (visible[i] == 0) {
-                    hidden = i;
-                    dropdowns[hidden].getParent().setVisible(true);
-                    dropdowns[hidden].getParent().setManaged(true);
-                    numVisible++;
-                    visible[i] = 1;
-                    return;
-                }
-            }
-        }
-    }
-
-    @FXML
-    void deleteDropDown(MouseEvent event) {
-        // make the appropriate nodes not visible and clear and selection for that dropdown
-        ((Node) event.getSource()).getParent().setVisible(false);
-        ((Node) event.getSource()).getParent().setManaged(false);
-        ((ComboBox<String>) ((Node) event.getSource()).getParent().getChildrenUnmodifiable().get(0)).getSelectionModel()
-                .clearSelection();
-        numVisible--;
-        for (int i = 0; i < deleteButtons.length; i++) {
-            if ((((Node) event.getSource()).getId()).equals(deleteButtons[i])) {
-                visible[i] = 0;
-                return;
-            }
-        }
-    }
-    
-    @FXML
-    void intializeMovementRule(MouseEvent event) {
-    	modifyTransitionButtons.setVisible(false);
-    	addTransition.setVisible(true);
-    }
-    
-    @FXML
-    void selectTransition(MouseEvent event) {
-    	addTransition.setVisible(false);
-    	modifyTransitionButtons.setVisible(true);
-    }
-    
-    @FXML
-    void changeTransition(MouseEvent event) {
-    	currentTransition = (currentTransition+1) % 3;
-    	transition.setText(transitions[currentTransition]);
-    }
-
-    @FXML
-    void deleteTransition(MouseEvent event) {
-    	transition.setVisible(false);
-    }
-
-    @FXML
-    public void changeAnchorScene(Event event) throws IOException {
-        Node node = (Node) event.getSource();
-        String tokenType = (String) node.getUserData();
-        Stage app_stage = (Stage) node.getScene().getWindow();
-
-        if (tokenType.equals("gameToken")) {
-            Parent addMovementPiece = FXMLLoader.load(getClass().getResource("Views/customToken.fxml"));
-            app_stage.setScene(new Scene(addMovementPiece));
-        }
-        else if (tokenType == ""){}
-        else {}
-    }
+    LocalStorage localStorage = LocalStorage.getInstance();
 
     //TODO
     /*
@@ -141,8 +26,9 @@ public class GameEditorMainController {
         Don't exit the scene until pressing the actual exit button.
         After save, can continue to edit. If saved, and editing, just re-edit in
         the database, rather than a new savedGame.
+
+        Saves the game and returns to main menu
      */
-    
     @FXML
     public void saveGame(Event event) throws IOException {
         Node node = (Node) event.getSource();
@@ -152,33 +38,41 @@ public class GameEditorMainController {
         if (gameName.equals("")) {
             gameName = "Game" + new Random().nextInt(10000);
         }
-        Token newgame = new Token(gameName);
-        JSONConverter savedGames = new JSONConverter(newgame, "test.json");
-        savedGames.To_JSON();
 
-
-        storeGameObjects();
+        storeGameObjects(gameName);
 
         popup(event, "Game has been saved");
-        exitToMainMenu(node);
-
+        exitToMainMenu(event);
     }
 
-    public void exitToMainMenu(Node node) throws IOException {
-        //change stage to mainMenu stage from gamePlay
-        Stage appStage = (Stage) node.getScene().getWindow();
+    //TODO modifying an existing game would open it up in localStorage, edit, and do the same thing
+    //TODO initialize localStorage
+    //exits to main menu and resets localStorage
+    @FXML
+    public void exitToMainMenu(Event event) throws IOException {
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("MainMenuFXML.fxml")));
         Scene scene = new Scene(root);
         appStage.setScene(scene);
         appStage.show();
+
+        //resets localStorage on exit
+        LocalStorage.reset();
     }
 
     //TODO getting the objects to save in the JSON
     //takes all the objects from localStorage and stores them in the JSON
-    public void storeGameObjects() {
-        LocalStorage localStorage = LocalStorage.LocalStorage();
+    public void storeGameObjects(String game) throws IOException {
         for (Map.Entry<String, Object> entry: localStorage.storage.entrySet()) {
-            System.out.println(entry.getValue());
+            Rule rule = new Rule();
+            rule.update_rng((RNG) entry.getValue());
+            Board boardTemp  = new Board();
+            boardTemp.add_rule(rule);
+
+            Token newgame = new Token();
+            newgame.update_gameboard(boardTemp);
+            JSONConverter savedGames = new JSONConverter(newgame, "test.json");
+            savedGames.To_JSON();
         }
     }
 
