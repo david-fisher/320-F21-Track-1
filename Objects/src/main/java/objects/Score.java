50406c54f9f17427ec3a40c5e5335079e3bde65d
Mehul1107 <43730457+Mehul1107@users.noreply.github.com>
2021-12-08 12:36:54 -0500

    Comments added

--- Objects/src/main/java/objects/Score.java
+++ Objects/src/main/java/objects/Score.java
@@ -1,5 +1,6 @@
 package src.main.java.objects;
 
+//Score class that can used to store and update a player's score
 public class Score extends Saveable {
     int cur_score;
     
@@ -13,6 +14,7 @@ public class Score extends Saveable {
         this.cur_score = score;
     }
     
+    //This is done so that the score can be initialized based on the player's ID
     public Score(String id, int score){
     	super(id);
         this.cur_score = score;
@@ -22,10 +24,8 @@ public class Score extends Saveable {
         return this.cur_score;
     }
     
+    //To update the score depending on the player's move
     public int update(int change){
-        //The score will be updated based on the rules that will be provided by Team 4
-        //Iterate through the rule list and change the score based on each rule
-        // int new_score = 1;//Calculate based on rules and update
         this.cur_score = change;
         return this.cur_score;
     }
