b860885a755810343edb99abfec750d21a28f84a
tomking <tomking127@gmail.com>
2021-12-11 21:58:21 -0500

    Fix GameObject contructor calls

--- src/Objects/Piece.java
+++ src/Objects/Piece.java
@@ -7,23 +7,23 @@ public class Piece extends GameObject {
     private transient ImageView image;
 
     public Piece(ArrayList<Rule> rules, Hashtable<String, String> attributes) {
-        super(rules, attributes);
+        super(rules, attributes, 0);
         this.image = new ImageView();
     }
 
     public Piece(int x, int y, ArrayList<Rule> rules, Hashtable<String, String> attributes) {
-        super(x, y, rules, attributes);
+        super(x, y, rules, attributes, 0);
         this.image = new ImageView();
     }
 
     public Piece(int x, int y, ArrayList<Rule> rules, Hashtable<String, String> attributes, ImageView image) {
-        super(x, y, rules, attributes);
+        super(x, y, rules, attributes, 0);
         this.image = image;
     }
 
     public Piece(String id, int x, int y, ArrayList<Rule> rules, Hashtable<String, String> attributes,
             ImageView image) {
-        super(id, x, y, rules, attributes);
+        super(id, x, y, rules, attributes, 0);
         this.image = image;
     }
 
