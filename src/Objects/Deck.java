841d4f2a227f0b899a4ca41e56cca0a0d6d170d1
Simon Nathans <simonmn99@gmail.com>
2021-12-11 21:03:23 -0500

    Fix Deck

--- src/Objects/Deck.java
+++ src/Objects/Deck.java
@@ -13,7 +13,7 @@ public class Deck extends Saveable {
         this.cards = new ArrayList<Card>();
     }
 
-    public Deck(ArrayList<Piece> cards) {
+    public Deck(ArrayList<Card> cards) {
         super();
         this.cards = cards;
     }
@@ -54,21 +54,20 @@ public class Deck extends Saveable {
     // Add a new card into the deck
     public ArrayList<Card> addCard(Card updatedCard) {
         try {
-            // If the card is found then it already exists
-            int index = this.cards.indexOf(findCardByID(new_piece.get_id()));
+            this.cards.indexOf(findCardByID(updatedCard.get_id()));
             System.out.println("Card already exists");
         } catch (Exception e) {
             // If there is an error finding the card then we know that it can be added
-            this.cards.add(new_piece);
+            this.cards.add(updatedCard);
         }
-        return this.pieces;
+        return this.cards;
     }
 
     // Update a card in the deck
     public ArrayList<Card> updateCard(Card newCard) {
         try {
             int index = this.cards.indexOf(findCardByID(newCard.get_id()));
-            this.pieces.set(index, newCard);
+            this.cards.set(index, newCard);
         } catch (Exception e) {
             System.out.println("Error: Card not found in deck.");
         }
@@ -76,7 +75,7 @@ public class Deck extends Saveable {
     }
 
     // Add a list of cards into the deck, takes an array list of cards to add
-    public ArrayList<Piece> addCards(ArrayList<Card> newCards) {
+    public ArrayList<Card> addCards(ArrayList<Card> newCards) {
         for (Card newCard : newCards) {
             addCard(newCard);
         }
@@ -84,7 +83,7 @@ public class Deck extends Saveable {
     }
 
     // remove a card from the deck, takes the card that needs removal of as parem
-    public ArrayList<Piece> removeCard(Card input) {
+    public ArrayList<Card> removeCard(Card input) {
         try {
             this.cards.remove(this.cards.indexOf(findCardByID(input.get_id())));
         } catch (Exception e) {
@@ -95,13 +94,13 @@ public class Deck extends Saveable {
     }
 
     // Clear all cards from deck
-    public ArrayList<Piece> removeCards() {
+    public ArrayList<Card> removeCards() {
         this.cards.clear();
         return this.cards;
     }
 
     // Find a card in the deck by ID, return null if not found
-    public Piece findCardByID(String ID) {
+    public Card findCardByID(String ID) {
         return this.cards.stream().filter(card -> card.get_id().equals(ID)).findFirst().orElse(null);
     }
 }
