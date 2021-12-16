4c38d39d60524bab8988c690acedab63d837bdf1
mmatthes98 <mmatthes@umass.edu>
2021-12-15 23:10:31 -0500

    Added more to choiceReader

--- src/gamePlay/boardGrid/choiceReader.java
+++ src/gamePlay/boardGrid/choiceReader.java
@@ -2,8 +2,16 @@ package gamePlay.boardGrid;
 
 import Objects.Deck;
 import Objects.Player;
+import Objects.Tile;
+import State.Choice;
+import State.GameState;
+import State.MoveChoice;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,16 +23,19 @@ public class choiceReader {
     private Button deck;
     private Button dice;
     private Button invButton;
-
+    private List<Choice> choiceList;
     private ArrayList<Integer[]> cordList;
-    private ArrayList<Deck> invDeck;
+    private GameState gameState;
 
-    public choiceReader(boardGrid board, List<Player> playerList, Button spinner, Button deck, Button dice){
+    public choiceReader(boardGrid board, List<Player> playerList, Button spinner, Button deck, Button dice, List<Choice> choiceList, Button invButton, GameState gameState){
         this.board = board;
         this.playerList = playerList;
         this.spinner = spinner;
         this.deck = deck;
         this.dice = dice;
+        this.choiceList = choiceList;
+        this.invButton = invButton;
+        this.gameState = gameState;
     }
 
     private void nullActions(){
@@ -35,5 +46,24 @@ public class choiceReader {
         deck.setOnAction(null);
         dice.setOnAction(null);
         invButton.setOnAction(null);
+    }
+
+    private void parseList(){
+        for(int i = 0; i < choiceList.size(); i++){
+            if(choiceList.get(0) instanceof MoveChoice){
+                move((MoveChoice) choiceList.get(i), i);
+            }
+        }
+    }
+
+    private void move(MoveChoice m, int choice){
+        Tile tile = m.getTile();
+        StackPane pane = board.getBoardCell(tile.getX(), tile.getY());
+        pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                gameState.progressGame(choice);
+            }
+        });
     }
 }
