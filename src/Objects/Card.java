8095073268b0a5c0328b22d83e96c570234c8d2e
Haijie Qin <jackqin2000@gmail.com>
2021-12-16 01:50:42 -0500

    Change original files back from a wrong attempt

--- src/Objects/Card.java
+++ src/Objects/Card.java
@@ -11,39 +11,39 @@ import javafx.scene.image.ImageView;
   When a Card is used, its Rules are executed and then it is discarded from the player's hand.
 */
 public class Card extends GameObject {
-  private transient ImageView image;
+    private transient ImageView image;
 
-  public Card() {
-    this(0, new ImageView());
-  }
+    public Card() {
+        this(0, new ImageView());
+    }
 
-  public Card(ImageView image) {
-    this(0, image);
-  }
+    public Card(ImageView image) {
+        this(0, image);
+    }
 
-  public Card(boolean rules, ImageView image) {
-    this(rules, 0, image);
-  }
+    public Card(ArrayList<Rule> rules, ImageView image) {
+        this(rules, 0, image);
+    }
 
-  public Card(int constant, ImageView image) {
-    this(new ArrayList<Rule>(), new int[]{constant}, image);
-  }
+    public Card(int constant, ImageView image) {
+        this(new ArrayList<Rule>(), constant, image);
+    }
 
-  public Card(int[] range, ImageView image) {
-    this(new ArrayList<Rule>(), range, image);
-  }
+    public Card(int[] range, ImageView image) {
+        this(new ArrayList<Rule>(), range, image);
+    }
 
-  public Card(boolean rules, int constant, ImageView image) {
-    super(rules, new int[]{constant});
-    this.image = image;
-  }
+    public Card(ArrayList<Rule> rules, int constant, ImageView image) {
+        super(rules, constant);
+        this.image = image;
+    }
 
-  public Card(ArrayList<Rule> rules, int[] range, ImageView image) {
-    super(rules, range);
-    this.image = image;
-  }
+    public Card(ArrayList<Rule> rules, int[] range, ImageView image) {
+        super(rules, range);
+        this.image = image;
+    }
 
-  public ImageView getImage() {
-    return this.image;
-  }
+    public ImageView getImage() {
+        return this.image;
+    }
 }
